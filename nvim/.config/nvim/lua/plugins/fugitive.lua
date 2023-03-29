56a65b9ef8cfc379fb24f0331801b75aa11ec447
mattstruble <matt.struble@nike.com>
2023-03-29 10:22:13 -0400

    Adding fugitive

---
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -0,0 +1,37 @@
+--------------------
+-- FUGITIVE
+-- https://github.com/ThePrimeagen/init.lua/blob/master/after/plugin/fugitive.lua
+--------------------
+
+return {
+	"tpope/vim-fugitive",
+	config = function()
+		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+		local mestruble_fugitive = vim.api.nvim_create_augroup("mestruble_fugitive", {})
+		local autocmd = vim.api.nvim_create_autocmd
+
+		autocmd("BufWinEnter", {
+			group = mestruble_fugitive,
+			pattern = "*",
+			callback = function()
+				if vim.bo.ft ~= "fugitive" then
+					return
+				end
+
+				local bufnr = vim.api.nvim_get_current_buf()
+				local opts = { buffer = bufnr, remap = false }
+
+				vim.keymap.set("n", "<leader>p", function()
+					vim.cmd.Git("push")
+				end, opts)
+
+				vim.keymap.set("n", "<leader>P", function()
+					vim.cmd.Git({ "pull", "--rebase" })
+				end, opts)
+
+				vim.keymap.set("n", "<leader>t", ":Git push -u origin", opts)
+			end,
+		})
+	end,
+}
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -18,7 +18,7 @@ return {
 		vim.keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
 		vim.keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
 		vim.keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
-		vim.keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+		-- vim.keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
 
 		local actions = require("telescope.actions")
 		local themes = require("telescope.themes")
