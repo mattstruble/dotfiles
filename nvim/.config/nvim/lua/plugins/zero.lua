d252018966612399abe817011579f59d3d56df10
mattstruble <matt.struble@nike.com>
2023-03-29 18:50:28 -0400

    Improved null-ls keybindings.

--- nvim/.config/nvim/lua/plugins/zero.lua
+++ nvim/.config/nvim/lua/plugins/zero.lua
@@ -89,18 +89,53 @@ return {
 			end,
 		})
 
+		local cmp_mappings = lsp.defaults.cmp_mappings({
+			["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+			["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+			["<C-Space>"] = cmp.mapping.complete(),
+		})
+
+		cmp_mappings["<Tab>"] = nil
+		cmp_mappings["<S-Tab>"] = nil
+
 		lsp.setup_nvim_cmp({
 			preselect = "none",
 			completion = {
 				completeopt = "menu, menuone, noinsert, noselect",
 			},
-			mapping = lsp.defaults.cmp_mappings({
-				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
-				["<C-Space>"] = cmp.mapping.complete(),
-			}),
+			mapping = cmp_mappings,
 		})
+
+		lsp.on_attach(function(client, bufnr)
+			local opts = { buffer = bufnr, remap = false }
+			vim.keymap.set("n", "gd", function()
+				vim.lsp.buf.definition()
+			end, opts)
+			vim.keymap.set("n", "gr", function()
+				vim.lsp.buf.references()
+			end, opts)
+			vim.keymap.set("n", "gD", function()
+				vim.lsp.buf.declaration()
+			end, opts)
+			vim.keymap.set("n", "K", function()
+				vim.lsp.buf.hover()
+			end, opts)
+			vim.keymap.set("n", "<leader>rn", function()
+				vim.lsp.buf.rename()
+			end, opts)
+			vim.keymap.set("n", "<leader>ca", function()
+				vim.lsp.buf.code_action()
+			end, opts)
+			vim.keymap.set("n", "]d", function()
+				vim.diagnostic.goto_prev()
+			end, opts)
+			vim.keymap.set("n", "[d", function()
+				vim.diagnostic.goto_next()
+			end, opts)
+			vim.keymap.set("n", "<C-h>", function()
+				vim.lsp.buf.signature_help()
+			end, opts)
+		end)
 
 		lsp.nvim_workspace()
 		lsp.setup()
