312a0319471b7d4f895cf1ff15c7254c954f805d
mattstruble <matt.struble@nike.com>
2023-05-11 13:20:27 -0400

    Cleanup noice

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -51,7 +51,21 @@ return {
 		},
 		config = function()
 			require("noice").setup({
-				presets = { inc_rename = true },
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true,
+					},
+				},
+				presets = {
+					inc_rename = true,
+					bottom_search = true,
+					command_palette = true,
+					long_message_to_split = true,
+					lsp_doc_border = true,
+				},
 			})
 		end,
 	},
