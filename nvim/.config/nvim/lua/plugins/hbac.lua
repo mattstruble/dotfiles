a1200608eb795e451874d7716d41ce632abb355d
mattstruble <matt.struble@nike.com>
2023-06-13 11:29:31 -0400

    Add heuristic buffer auto-close

---
+++ nvim/.config/nvim/lua/plugins/hbac.lua
@@ -0,0 +1,12 @@
+return {
+	"axkirillov/hbac.nvim",
+	dependencies = {
+		-- these are optional, add them, if you want the telescope module
+		"nvim-telescope/telescope.nvim",
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("hbac").setup()
+	end,
+}
