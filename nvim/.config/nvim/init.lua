5fd1a0cbcfbbdc616039d8cd32389e8616598840
mattstruble <matt.struble@nike.com>
2023-06-13 17:17:16 -0400

    Add command to keep focus location between windows

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -25,3 +25,14 @@ opts = {
 }
 
 require("lazy").setup("plugins", opts)
+
+-- Automatically jump to the last cursor spot in file before exiting
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
