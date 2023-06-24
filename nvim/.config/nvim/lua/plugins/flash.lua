1a1e4abd8a278fb060247cbef4f38e0941f1a4b1
Matt Struble <mattstruble@outlook.com>
2023-06-24 11:23:04 -0400

    Add flash and eyeliner

---
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,41 @@
+return {
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "o", "x" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+		},
+	},
+	{
+		"jinh0/eyeliner.nvim",
+		event = "VeryLazy",
+		opts = {
+			highlight_on_key = true, -- show highlights only after keypress
+			dim = true, -- dim all other characters if set to true (recommended!)
+		},
+	},
+}
