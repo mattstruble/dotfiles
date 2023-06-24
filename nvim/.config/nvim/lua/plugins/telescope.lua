4ed20675c96d4eab6e5ea25dfa5676914208de80
Matt Struble <mattstruble@outlook.com>
2023-06-24 11:28:18 -0400

    Adding project

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -31,6 +31,21 @@ return {
 		},
 	},
 	{
+		"ahmedkhalf/project.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+		},
+		lazy = true,
+		opts = {},
+		config = function(_, opts)
+			require("project_nvim").setup(opts)
+			require("telescope").load_extension("projects")
+		end,
+		keys = {
+			{ "<leader>fp", "<Cmd>Telescope projects<CR>", desc = "Projects" },
+		},
+	},
+	{
 		"nvim-telescope/telescope.nvim",
 		lazy = true,
 		dependencies = {
