4ff4a843e38519144fb39f89be2ef75c02a71620
Matt Struble <mattstruble@outlook.com>
2023-06-24 17:22:47 -0400

    Use flash as a dependency for eyeliner so that it is preferred

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -32,6 +32,9 @@ return {
 	},
 	{
 		"jinh0/eyeliner.nvim",
+		dependencies = {
+			"folke/flash.nvim",
+		},
 		event = "VeryLazy",
 		opts = {
 			highlight_on_key = true, -- show highlights only after keypress
