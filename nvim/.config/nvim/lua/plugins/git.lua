827792b411eaa274f91f9ade1fe8a5f16c5fe36c
mattstruble <matt.struble@nike.com>
2023-08-23 10:06:55 -0400

    chore(git): combine into one file

---
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -0,0 +1,72 @@
+return {
+	{
+		"wintermute-cell/gitignore.nvim",
+		lazy = true,
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+		},
+		keys = {
+			{ "<leader>gi", "<cmd>Gitignore<CR>", desc = "Generate gitignore" },
+		},
+	},
+	{
+		"pwntester/octo.nvim",
+		lazy = true,
+		cmd = "Octo",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("octo").setup()
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+		lazy = true,
+		cmd = "Git",
+		keys = {
+			{ "<leader>gs", vim.cmd.Git, desc = "Git fugitive" },
+		},
+		config = function()
+			local mestruble_fugitive = vim.api.nvim_create_augroup("mestruble_fugitive", {})
+			local autocmd = vim.api.nvim_create_autocmd
+
+			autocmd("BufWinEnter", {
+				group = mestruble_fugitive,
+				pattern = "*",
+				callback = function()
+					if vim.bo.ft ~= "fugitive" then
+						return
+					end
+
+					local bufnr = vim.api.nvim_get_current_buf()
+					local opts = { buffer = bufnr, remap = false }
+
+					vim.keymap.set("n", "<leader>cc", function()
+						vim.cmd.Git("commit -s")
+					end, opts)
+
+					vim.keymap.set("n", "<leader>p", function()
+						vim.cmd.Git("push")
+					end, opts)
+
+					vim.keymap.set("n", "<leader>P", function()
+						vim.cmd.Git({ "pull", "--rebase" })
+					end, opts)
+
+					vim.keymap.set("n", "<leader>t", ":Git push -u origin", opts)
+				end,
+			})
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		lazy = true,
+		event = "VeryLazy",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+}
