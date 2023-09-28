04e7bcde4d776ca125bac693d5b6ebea0f9e3759
mattstruble <matt.struble@nike.com>
2023-09-28 10:25:55 -0400

    fix(symbol-usage): change position to eol
    Signed-off-by: mattstruble <matt.struble@nike.com>

--- nvim/.config/nvim/lua/plugins/editor/symbol-usage.lua
+++ nvim/.config/nvim/lua/plugins/editor/symbol-usage.lua
@@ -3,7 +3,7 @@ return {
 	name = "symbol-usage",
 	event = "BufReadPre", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
 	opts = {
-		vt_position = "textwidth",
+		vt_position = "end_of_line",
 		request_pending_text = false,
 	},
 }
