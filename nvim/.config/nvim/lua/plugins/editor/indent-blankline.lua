d006752e7dd30ed457df9817063bc1118bf1dc61
mattstruble <matt.struble@nike.com>
2024-02-28 11:23:57 -0500

    chore(indent-blankline): change carriage return

--- nvim/.config/nvim/lua/plugins/editor/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/editor/indent-blankline.lua
@@ -11,7 +11,7 @@ return {
 			vim.opt.termguicolors = true
 			vim.opt.list = true
 			vim.opt.listchars:append("space:⋅")
-			vim.opt.listchars:append("eol:↴")
+			vim.opt.listchars:append("eol:󰌑")
 
 			require("ibl").setup({
 				-- show_end_of_line = true,
