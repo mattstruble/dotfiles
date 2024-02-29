ccc1c7091d6455b29b7d8695728a0db1e3a01003
mattstruble <matt.struble@nike.com>
2024-02-29 09:04:29 -0500

    feat: rust

---
+++ nvim/.config/nvim/lua/plugins/lang/rust.lua
@@ -0,0 +1,66 @@
+local py = require("utils.python")
+
+return {
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			table.insert(opts.ensure_installed, "rust-analyzer")
+			table.insert(opts.ensure_installed, "codelldb")
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			table.insert(opts.ensure_installed, "rust")
+		end,
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		opts = function(_, opts)
+			local nls = require("null-ls")
+
+			table.insert(opts.sources, nls.builtins.formatting.rustfmt)
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				["rust-analyzer"] = {
+					diagnostics = {
+						enable = true,
+					},
+				},
+			},
+		},
+	},
+    {
+        "simrat39/rust-tools.nvim",
+        ft = "rust",
+        config = function()
+            local rt = require("rust-tools")
+            rt.sertup({
+                on_attach = function(_, bufnr)
+                    vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr})
+                    vim.keymap.set("n", "<leader>a", rt.code_action_group.code_action_group, { buffer = bufnr})
+                end
+            })
+        end
+
+    },
+    {
+        "mfussenegger/nvim-dap",
+        opts = function(_, opts)
+            opts.configurations.rust = {
+                name = "Launch file",
+                type = "codelldb",
+                request = "launch",
+                program = function()
+                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. /, 'file')
+                end,
+                cwd = '${workspaceFolder}',
+                stopOnEntry = false
+            }
+        end
+    }
+}
