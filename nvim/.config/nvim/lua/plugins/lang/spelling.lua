25096a99058a09f8f6a1639379560a049769531c
mattstruble <matt.struble@nike.com>
2024-03-01 16:08:48 -0500

    fix: disable codespell on format

--- nvim/.config/nvim/lua/plugins/lang/spelling.lua
+++ nvim/.config/nvim/lua/plugins/lang/spelling.lua
@@ -10,7 +10,7 @@ return {
 		opts = function(_, opts)
 			local nls = require("null-ls")
 
-			table.insert(opts.sources, nls.builtins.formatting.codespell)
+			-- table.insert(opts.sources, nls.builtins.formatting.codespell)
 			table.insert(opts.sources, nls.builtins.formatting.trim_whitespace)
 			table.insert(opts.sources, nls.builtins.diagnostics.codespell)
 			table.insert(opts.sources, nls.builtins.diagnostics.write_good)
