d484cfdea3f1112fa2dc0e459710beaaa6869525
mattstruble <matt.struble@nike.com>
2024-03-07 14:03:53 -0500

    fix: tmux and lualine to align

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -15,11 +15,54 @@ return {
 			"nvim-tree/nvim-web-devicons",
 			"bluz71/vim-nightfly-guicolors",
 			"meuter/lualine-so-fancy.nvim",
+			"folke/noice.nvim",
 			-- "roobert/statusline-action-hints.nvim",
 		},
-		config = function()
-			require("plugins.lualine.config")
-		end,
+		after = "noice.nvim",
+		opts = {
+			options = {
+				icons_enabled = true,
+				component_separators = "|",
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = {
+					{
+						"mode",
+						fmt = function()
+							return "󰀘"
+						end,
+						separator = { left = "" },
+						right_padding = 2,
+					},
+				},
+				lualine_b = {
+					{ "fancy_branch" },
+					{ "fancy_diff" },
+				},
+				lualine_c = {},
+				lualine_x = {
+					-- { require("statusline-action-hints").statusline },
+					{ "fancy_diagnostics" },
+					{ "fancy_searchcount" },
+					{ "fancy_location" },
+				},
+				lualine_y = {
+					{ "fancy_filetype", ts_icon = "" },
+				},
+				lualine_z = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+					{ separator = { right = "" }, left_padding = 2 },
+				},
+			},
+			extensions = {
+				"nvim-tree",
+			},
+		},
 	},
 	{
 		"rcarriga/nvim-notify",
