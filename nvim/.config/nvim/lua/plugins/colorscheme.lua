915f1b89b39dfb952b7ccf67260583f374f07e4d
mattstruble <matt.struble@nike.com>
2024-03-07 11:10:31 -0500

    Unify colorscheme to catppuccin-mocha because I'm lazy

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,7 @@ return {
 		"bluz71/vim-nightfly-colors",
 		name = "nightfly",
 		lazy = false,
+		enabled = false,
 		priority = 1000,
 		config = function()
 			vim.cmd.colorscheme("nightfly")
@@ -13,64 +14,9 @@ return {
 		name = "catppuccin",
 		priority = 1000,
 		lazy = false,
-		enabled = false,
+		enabled = true,
 		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha",
-				background = {
-					light = "latte",
-					dark = "mocha",
-				},
-				transparent_background = false,
-				show_end_of_buffer = true,
-				term_colors = true,
-				dim_inactive = {
-					enabled = false,
-				},
-				no_italic = false,
-				no_bold = false,
-				styles = {
-					comments = { "italic" },
-					properties = {},
-					functions = { "bold" },
-					keywords = {},
-					operators = { "bold" },
-					conditionals = { "bold" },
-					loops = { "bold" },
-					booleans = { "bold" },
-					numbers = {},
-					types = {},
-					strings = {},
-					variables = {},
-				},
-				custom_highlights = {},
-				integrations = {
-					native_lsp = {
-						enabled = true,
-						virtual_text = {
-							errors = {},
-							hints = {},
-							warnings = {},
-							information = {},
-						},
-						underlines = {
-							errors = { "undercurl" },
-							hints = { "undercurl" },
-							warnings = { "undercurl" },
-							information = { "undercurl" },
-						},
-					},
-					indent_blankline = { enabled = true, colored_indent_levels = true },
-					nvimtree = true,
-					which_key = true,
-					cmp = true,
-					gitsigns = true,
-					nvimtree = true,
-					telescope = true,
-					notify = true,
-				},
-			})
-			vim.cmd.colorscheme("catppuccin")
+			vim.cmd.colorscheme("catppuccin-mocha")
 		end,
 	},
 }
