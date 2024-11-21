09b172b6ac98fe66ede5e618dfed3c2358d7fbb2
mattstruble <4325029+mattstruble@users.noreply.github.com>
2024-11-21 12:58:21 -0500

    fix: nvim-cmp min snippet length
    - https://old.reddit.com/r/neovim/comments/1gwfunb/this_is_nigh_unusable_every_keystroke_regardless/
    
    Signed-off-by: mattstruble <4325029+mattstruble@users.noreply.github.com>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -66,6 +66,10 @@ require("lazy").setup({
 				local cmp = require("cmp")
 				local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
+				opts.sources = {
+					{ name = "snippets", keyword_length = 3 },
+				}
+
 				opts.mapping["<C-p>"] = cmp.mapping.select_prev_item(cmp_select)
 				opts.mapping["<C-n>"] = cmp.mapping.select_next_item(cmp_select)
 				opts.mapping["<C-Space>"] = cmp.mapping.complete()
