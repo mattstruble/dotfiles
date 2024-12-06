f7accaf064c6d556f99061a56fcfd02c9296a2ed
mattstruble <4325029+mattstruble@users.noreply.github.com>
2024-12-06 16:40:11 -0500

    feat: add snacks scratch and update packages

--- nvim/.config/nvim/lua/plugins/editor/snacks.lua
+++ nvim/.config/nvim/lua/plugins/editor/snacks.lua
@@ -38,6 +38,27 @@ return {
 	},
 	keys = {
 		{
+			"<leader>.",
+			function()
+				Snacks.scratch()
+			end,
+			desc = "Toggle Scratch Buffer",
+		},
+		{
+			"<leader>S",
+			function()
+				Snacks.scratch.select()
+			end,
+			desc = "Select Scratch Buffer",
+		},
+		{
+			"<leader>n",
+			function()
+				Snacks.notifier.show_history()
+			end,
+			desc = "Notification History",
+		},
+		{
 			"<leader>un",
 			function()
 				Snacks.notifier.hide()
