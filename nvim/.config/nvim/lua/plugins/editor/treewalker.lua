fbe906273ca1655e5444ba9baa4621e50a07fdcf
mattstruble <4325029+mattstruble@users.noreply.github.com>
2024-12-13 09:49:50 -0500

    feat: add treewalker

---
+++ nvim/.config/nvim/lua/plugins/editor/treewalker.lua
@@ -0,0 +1,9 @@
+return {
+	"aaronik/treewalker.nvim",
+	keys = {
+		{ "<C-d>", ":Treewalker Down<cr>", desc = "Move Down to next neighbor node" },
+		{ "<C-f>", ":Treewalker Up<cr>", desc = "Move Up to next neighbor node" },
+		{ "<C-s>", ":Treewalker Left<cr>", desc = "Move Left to next neighbor node" },
+		{ "<C-g>", ":Treewalker Right<cr>", desc = "Move Right to next neighbor node" },
+	},
+}
