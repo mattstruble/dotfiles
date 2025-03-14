65769f7eefda7a05a83e8fb86870a17561b96c18
Matt Struble <matt.struble@nike.com>
2025-03-14 12:09:47 -0400

    chore: update fzf lua config
    Signed-off-by: Matt Struble <matt.struble@nike.com>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -113,6 +113,16 @@ require("lazy").setup({
                 defaults = {
                     hidden = true,
                 },
+                oldfiles = {
+                    cwd_only = true,
+                    stat_file = true, -- verify files exist on disk
+                    include_current_session = true,
+                },
+                previewers = {
+                    builtin = {
+                        syntax_limit_b = 1024 * 100,
+                    },
+                },
             },
         },
 
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,12 @@
 return {
-	{
-		"bluz71/vim-nightfly-colors",
-		name = "nightfly",
-		lazy = false,
-		enabled = false,
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("nightfly")
-		end,
-	},
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		lazy = false,
-		enabled = true,
-		config = function()
-			vim.cmd.colorscheme("catppuccin-mocha")
-		end,
-	},
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        lazy = false,
+        enabled = true,
+        config = function()
+            vim.cmd.colorscheme("catppuccin-mocha")
+        end,
+    },
 }
