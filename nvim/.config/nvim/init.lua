d6ba753360a66d66b2bfc71035796ed86b12cbcf
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2025-03-28 11:21:57 -0400

    chore: remove duplicate diagnostics in favor of lazyvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -251,12 +251,6 @@ require("lazy").setup({
     },
 }, opts)
 
--- Diagnostics
-vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = " ", texthl = "DiagnosticSignHint" })
-
 -- Automatically jump to the last cursor spot in file before exiting
 vim.api.nvim_create_autocmd("BufReadPost", {
     callback = function()
