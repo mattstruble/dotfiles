513160ed4d707a5f662f1c33fc2ec8df2b319e04
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2025-03-31 15:48:24 -0400

    chore: fix tex

--- nvim/.config/nvim/lua/plugins/lang/latex.lua
+++ nvim/.config/nvim/lua/plugins/lang/latex.lua
@@ -1,48 +1,48 @@
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = { "*.tex" },
-	desc = "Set jupyter notebook file types",
-	command = "setfiletype tex",
+    pattern = { "*.tex" },
+    desc = "Set jupyter notebook file types",
+    command = "setfiletype tex",
 })
 
 return {
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"ltex-ls",
-			},
-		},
-	},
-	{
-		"mfussenegger/nvim-lint",
-		opts = {
-			linters_by_ft = {
-				python = { "lacheck" },
-			},
-		},
-	},
-	{
-		"stevearc/conform.nvim",
-		opts = {
-			formatters_by_ft = {
-				tex = { "tex-fmt" },
-			},
-		},
-	},
-	{
-		"abeleinin/papyrus",
-		lazy = true,
-		ft = "tex",
-		config = function()
-			vim.g.papyrus_latex_engine = "pdflatex"
-			vim.g.papyrus_viewer = "zathura"
-		end,
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "ltex-ls",
+            },
+        },
+    },
+    {
+        "mfussenegger/nvim-lint",
+        opts = {
+            linters_by_ft = {
+                tex = { "lacheck" },
+            },
+        },
+    },
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                tex = { "tex-fmt" },
+            },
+        },
+    },
+    {
+        "abeleinin/papyrus",
+        lazy = true,
+        ft = "tex",
+        config = function()
+            vim.g.papyrus_latex_engine = "pdflatex"
+            vim.g.papyrus_viewer = "zathura"
+        end,
         -- stylua: ignore
         keys = {
-            { "<leader>pc", ":PapyrusCompile<cr>", desc = "Papyrus Compile"},
-            { "<leader>pa", ":PapyrusAutoCompile<cr>", desc = "Papyrus Auto Compile"},
-            { "<leader>pv", ":PapyrusView<cr>", desc = "Papyrus View"},
-            { "<leader>ps", ":PapyrusStart<cr>", desc = "Papyrus Start"}
+            { "<leader>pc", ":PapyrusCompile<cr>",     desc = "Papyrus Compile" },
+            { "<leader>pa", ":PapyrusAutoCompile<cr>", desc = "Papyrus Auto Compile" },
+            { "<leader>pv", ":PapyrusView<cr>",        desc = "Papyrus View" },
+            { "<leader>ps", ":PapyrusStart<cr>",       desc = "Papyrus Start" }
         },
-	},
+    },
 }
