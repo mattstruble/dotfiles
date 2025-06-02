c187ba40fa2fa057964a82ff10268dfa9032c5f2
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2025-06-02 11:26:09 -0400

    chore: add work username
    Signed-off-by: Matt Struble <4325029+mattstruble@users.noreply.github.com>

--- nix-darwin/hosts/APKQTFWJ12ED96/home.nix
+++ nix-darwin/hosts/APKQTFWJ12ED96/home.nix
@@ -1,27 +1,33 @@
 let
-  home = builtins.getEnv "HOME";
+  userName = import ./username.nix;
+  home = "/Users/${userName}";
 in
 {
-  imports = [ ../../profiles/macos.nix ];
-  home = {
-    sessionVariables = {
-      OBSIDIAN_VAULT = "${home}/Library/CloudStorage/Box-Box/Obsidian/Vault";
-      NODE_EXTRA_CA_CERTS = "${home}/.ssl-cert/Nike-Root-Authority-NG.pem";
-      REQUESTS_CA_BUNDLE = "${home}/.ssl-cert/Nike-Root-Authority-NG.pem";
-      SSL_CERT_FILE = "${home}/.ssl-cert/Nike-Root-Authority-NG.pem";
-    };
-  };
-  programs = {
-    git = {
-      userName = "Matt Struble";
-      userEmail = "matt.struble@nike.com";
-      extraConfig = {
-        github.user = "mstru3_nike";
+  "${userName}" = {
+    imports = [
+      ../../profiles/macos.nix
+      ../../home.nix
+    ];
+    home = {
+      sessionVariables = {
+        OBSIDIAN_VAULT = "${home}/Library/CloudStorage/Box-Box/Obsidian/Vault";
+        NODE_EXTRA_CA_CERTS = "${home}/.ssl-cert/Nike-Root-Authority-NG.pem";
+        REQUESTS_CA_BUNDLE = "${home}/.ssl-cert/Nike-Root-Authority-NG.pem";
+        SSL_CERT_FILE = "${home}/.ssl-cert/Nike-Root-Authority-NG.pem";
       };
     };
-    zsh = {
-      shellAliases = {
-        gimme-aws-creds = "gimme-aws-creds --mfa-code `op item get nikecloud.com --otp`";
+    programs = {
+      git = {
+        userName = "Matt Struble";
+        userEmail = "matt.struble@nike.com";
+        extraConfig = {
+          github.user = "mstru3_nike";
+        };
+      };
+      zsh = {
+        shellAliases = {
+          gimme-aws-creds = "gimme-aws-creds --mfa-code `op item get nikecloud.com --otp`";
+        };
       };
     };
   };
