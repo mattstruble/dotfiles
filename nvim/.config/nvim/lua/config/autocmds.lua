3de3bbca8c55b4a31b79c48e7c15850fe311a3cd
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2025-07-11 11:08:07 -0400

    fix: remove ruff lsp hover in favor of basedpyright

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -151,3 +151,21 @@ A.nvim_create_autocmd("LspAttach", {
         })
     end,
 })
+
+-- Disable ruff hover in favor of basedpyright
+-- https://docs.astral.sh/ruff/editors/setup/#neovim
+A.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup(
+        "lsp_attach_disable_ruff_hover",
+        { clear = true }
+    ),
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client == nil then return end
+        if client.name == "ruff" then
+            -- Disable hover
+            client.server_capabilities.hoverProvider = false
+        end
+    end,
+    desc = "LSP: Disable hover capability from Ruff",
+})
