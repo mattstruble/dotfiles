a2a274308191a6b76cf422416acda88c98856ab6
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2025-07-11 10:49:41 -0400

    fix: remove extra python venv work to clean up basedpyright

--- nvim/.config/nvim/lsp/basedpyright.lua
+++ nvim/.config/nvim/lsp/basedpyright.lua
@@ -14,13 +14,5 @@ return {
                 },
             },
         },
-        python = {
-            pythonPath = py.venv(vim.fn.getcwd()),
-            venvPath = py.venv(vim.fn.getcwd()),
-        },
     },
-    on_new_config = function(new_config, new_root_dir)
-        py.env(new_root_dir)
-        new_config.settings.python.venvPath = py.venv(new_root_dir)
-    end,
 }
