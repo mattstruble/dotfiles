d5414036537504709cc3406600043231e070979f
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2025-07-11 10:48:55 -0400

    fix: basedpyright to play better with ruff

--- nvim/.config/nvim/lsp/basedpyright.lua
+++ nvim/.config/nvim/lsp/basedpyright.lua
@@ -1,12 +1,16 @@
 local py = require("utils.python")
-
+-- config help taken from https://github.com/astral-sh/ruff-lsp/issues/384
 return {
     settings = {
         basedpyright = {
+            disableOrganzieImports = true, -- Using Ruff
+            disableTaggedHints = true,
             analysis = {
+                ignore = { "*" }, -- Using Ruff
                 extraPaths = py.pep582(vim.fn.getcwd()),
                 diagnosticSeverityOverrides = {
                     reportUnusedCallResult = false,
+                    reportUndefinedVariable = "none",
                 },
             },
         },
