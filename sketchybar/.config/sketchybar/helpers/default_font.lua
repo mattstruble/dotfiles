15f18503c8e971757646c43cf0688612fb89f30d
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2025-10-06 09:53:29 -0400

    feat: update sketchybar to match felixkratz dotfiles

---
+++ sketchybar/.config/sketchybar/helpers/default_font.lua
@@ -0,0 +1,13 @@
+return {
+	text = "Iosevka Nerd Font", -- Used for text
+	numbers = "Iosevka Nerd Font Mono", -- Used for numbers
+
+	-- Unified font style map
+	style_map = {
+		["Regular"] = "Regular",
+		["Semibold"] = "Semibold",
+		["Bold"] = "Bold",
+		["Heavy"] = "Heavy",
+		["Black"] = "Black",
+	},
+}
