e687c94e40c2a564aea184a2c3c47d2e037a6891
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2026-03-13 14:53:59 -0400

    fix(llama): accept key to alt-space

--- nvim/.config/nvim/lua/plugins/coding/copilot.lua
+++ nvim/.config/nvim/lua/plugins/coding/copilot.lua
@@ -2,8 +2,9 @@ return {
     "ggml-org/llama.vim",
     init = function()
         vim.g.llama_config = {
-            show_info = 2,
+            show_info = 0,
             auto_fim = true,
+            keymap_fim_accept = "<A-Space>",
             keymap_fim_trigger = "<A-f>",
             keymap_fim_accept_word = "<A-w>",
         }
