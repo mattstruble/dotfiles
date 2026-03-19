3eaf59d6ac72737a7190708df41551e02e331348
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2026-03-19 12:06:28 -0400

    chore: remove rust-tools.nvim, clean up nix dead code and sort order

--- nvim/.config/nvim/lua/plugins/lang/rust.lua
+++ nvim/.config/nvim/lua/plugins/lang/rust.lua
@@ -33,27 +33,4 @@ return {
             },
         },
     },
-    {
-        "simrat39/rust-tools.nvim",
-        ft = "rust",
-        config = function()
-            local rt = require("rust-tools")
-            rt.setup({
-                on_attach = function(_, bufnr)
-                    vim.keymap.set(
-                        "n",
-                        "<C-space>",
-                        rt.hover_actions.hover_actions,
-                        { buffer = bufnr }
-                    )
-                    vim.keymap.set(
-                        "n",
-                        "<leader>a",
-                        rt.code_action_group.code_action_group,
-                        { buffer = bufnr }
-                    )
-                end,
-            })
-        end,
-    },
 }
