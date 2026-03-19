3814a2e9dc133e9c183e6536e1858090430d98c1
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2026-03-19 09:06:33 -0400

    feat(nvim): add gitsigns hunk keymaps and ih text object
    Configure on_attach with full LazyVim-parity bindings: ]h/[h
    navigation, <leader>gh* stage/reset/preview/blame/diff actions,
    and ih visual/operator-pending text object for hunk selection.

--- nvim/.config/nvim/lua/plugins/lang/git.lua
+++ nvim/.config/nvim/lua/plugins/lang/git.lua
@@ -45,10 +45,56 @@ return {
 
 	{
 		"lewis6991/gitsigns.nvim",
-		lazy = true,
-		event = "VeryLazy",
-		config = function()
-			require("gitsigns").setup()
-		end,
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			on_attach = function(buffer)
+				local gs = require("gitsigns")
+
+				local function map(mode, l, r, desc)
+					vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+				end
+
+				-- Navigation
+				map("n", "]h", function()
+					gs.nav_hunk("next")
+				end, "Next Hunk")
+				map("n", "[h", function()
+					gs.nav_hunk("prev")
+				end, "Prev Hunk")
+				map("n", "]H", function()
+					gs.nav_hunk("last")
+				end, "Last Hunk")
+				map("n", "[H", function()
+					gs.nav_hunk("first")
+				end, "First Hunk")
+
+				-- Actions
+				map("n", "<leader>ghs", gs.stage_hunk, "Stage Hunk")
+				map("n", "<leader>ghr", gs.reset_hunk, "Reset Hunk")
+				map("v", "<leader>ghs", function()
+					gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, "Stage Hunk")
+				map("v", "<leader>ghr", function()
+					gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, "Reset Hunk")
+				map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+				map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+				map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+				map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
+				map("n", "<leader>ghb", function()
+					gs.blame_line({ full = true })
+				end, "Blame Line")
+				map("n", "<leader>ghB", function()
+					gs.blame()
+				end, "Blame Buffer")
+				map("n", "<leader>ghd", gs.diffthis, "Diff This")
+				map("n", "<leader>ghD", function()
+					gs.diffthis("~")
+				end, "Diff This ~")
+
+				-- Text object
+				map({ "o", "x" }, "ih", gs.select_hunk, "GitSigns Select Hunk")
+			end,
+		},
 	},
 }
