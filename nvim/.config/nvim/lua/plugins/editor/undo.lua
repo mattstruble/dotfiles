023b32ece5aefb573f20ce35047bcf33cd3083ae
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2026-03-19 16:04:28 -0400

    chore(nvim): remove overlapping plugins and consolidate features
    Remove highlight-undo.nvim (consolidated into tiny-glimmer undo/redo),
    arrow.nvim and maple.nvim (user prefers built-in vim marks), and the
    entire jupyter lang pack (NotebookNavigator + iron.nvim + hydra.nvim +
    mini.comment -- all had maintenance issues, workflow unused).

--- nvim/.config/nvim/lua/plugins/editor/undo.lua
+++ nvim/.config/nvim/lua/plugins/editor/undo.lua
@@ -4,14 +4,6 @@
 
 return {
 	{
-		"tzachar/highlight-undo.nvim",
-		lazy = true,
-		event = { "BufReadPost", "BufNewFile" },
-		config = function()
-			require("highlight-undo").setup({})
-		end,
-	},
-	{
 		"mbbill/undotree",
 		lazy = true,
 		keys = {
