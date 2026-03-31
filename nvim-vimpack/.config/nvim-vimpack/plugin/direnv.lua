05b050b59542225346c866f1cc1b680c9c840dfd
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2026-03-31 09:37:07 -0400

    fix(nvim): guard direnv LspRestart with pcall (command may not exist yet)

--- nvim-vimpack/.config/nvim-vimpack/plugin/direnv.lua
+++ nvim-vimpack/.config/nvim-vimpack/plugin/direnv.lua
@@ -2,6 +2,8 @@ vim.pack.add({ "https://github.com/actionshrimp/direnv.nvim" })
 require("direnv-nvim").setup({
     async = true,
     on_direnv_finished = function()
-        vim.cmd("LspRestart")
+        -- Guard: LspRestart may not exist if lspconfig hasn't registered commands yet
+        -- (e.g., direnv fires on first BufEnter before any LSP attaches)
+        pcall(vim.cmd, "LspRestart")
     end,
 })
