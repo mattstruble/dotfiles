0241e08dfaeffc533fae4d791a3d6f4760ba1e0b
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2026-03-31 14:19:48 -0400

    feat(nvim): replace symbol-usage with native 0.12 codelens
    Remove symbol-usage.nvim plugin, clean stale persisted.nvim from
    lock file, enable vim.lsp.codelens in LspAttach autocmd.

--- nvim-vimpack/.config/nvim-vimpack/lua/config/autocmds.lua
+++ nvim-vimpack/.config/nvim-vimpack/lua/config/autocmds.lua
@@ -184,6 +184,9 @@ A.nvim_create_autocmd("LspAttach", {
         -- Reference word navigation (Snacks words)
         map("n", "<c-n>", function() Snacks.words.jump(vim.v.count1, true) end, "Next Reference")
         map("n", "<c-p>", function() Snacks.words.jump(-vim.v.count1, true) end, "Prev Reference")
+
+        -- Enable code lens (virtual line display, 0.12 native)
+        vim.lsp.codelens.enable(true, { bufnr = bufnr })
     end,
 })
 
--- nvim-vimpack/.config/nvim-vimpack/plugin/symbol-usage.lua
+++
@@ -1,5 +0,0 @@
-vim.pack.add({ "https://github.com/Wansmer/symbol-usage.nvim" })
-require("symbol-usage").setup({
-    vt_position = "end_of_line",
-    request_pending_text = false,
-})
