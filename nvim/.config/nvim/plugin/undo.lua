e11fb7f821bd0bdf2e002ece0d840d2396aaf309
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2026-04-01 09:33:10 -0400

    feat: undotree

--- nvim/.config/nvim/plugin/undo.lua
+++ nvim/.config/nvim/plugin/undo.lua
@@ -1,15 +1,8 @@
--- Tier 3: undotree (not built-in in stable 0.12 despite changelog)
-local loaded = false
-local function ensure_loaded()
-    if loaded then return end
-    loaded = true
-    local ok, err = pcall(vim.pack.add, { "https://github.com/mbbill/undotree" })
-    if not ok then
-        vim.notify("undotree: " .. tostring(err), vim.log.levels.ERROR)
-    end
-end
+vim.cmd("packadd nvim.undotree")
 
-vim.keymap.set("n", "<leader>uu", function()
-    ensure_loaded()
-    vim.cmd("UndotreeToggle")
-end, { desc = "Undo Tree" })
+vim.keymap.set(
+    "n",
+    "<leader>u",
+    require("undotree").open,
+    { desc = "Undo Tree" }
+)
