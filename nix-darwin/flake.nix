762958624d21e6afb87ca686eef6c8fec5d0d1f3
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2026-07-06 15:59:44 -0400

    feat(nix): add zen-browser flake input

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -42,6 +42,14 @@
 
     determinate.url = "github:DeterminateSystems/determinate";
 
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
+
   };
 
   outputs =
